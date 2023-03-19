0090f6f68a496026a0e947411b62c2d5fb49a417
Alberto Valenzuela <avalen28@gmail.com>
2023-03-19 21:12:33 +0100

    adds first call to backend

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -1,13 +1,25 @@
-import React, { useState, useEffect } from 'react'
-import showService from '../services/showService'
-import { Link } from 'react-router-dom'
+import React, { useState, useEffect } from "react";
+import showService from "../services/showService";
+import { Link } from "react-router-dom";
 
 export default function Home() {
+  const getData = async () => {
+    try {
+      const response = await showService.getShows();
+      console.log(response);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  useEffect(() => {
+    getData();
+  }, []);
 
   return (
     <div>
       <h2>Home</h2>
       {/* ITERATION 2: Should display a list of all the shows */}
     </div>
-  )
+  );
 }
